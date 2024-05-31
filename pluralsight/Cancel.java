023fdacb59077d2b2f04b6530075c7395db9fcd5
Jappdev9 <167275833+Jappdev9@users.noreply.github.com>
2024-05-31 10:12:16 -0400

    Delete pluralsight directory

--- pluralsight/Cancel.java
+++
@@ -1,9 +0,0 @@
-package com.pluralsight;
-
-public class Cancel {
-        public static void cancelOrder() {
-            // Logic to cancel the order
-            System.out.println("Order canceled successfully.");
-        }
-    }
-}
